b75eaa5df405913f411941f4d1d8555928a9cd65
agiskorska <skorskaga@gmail.com>
2022-09-15 07:14:51 +0100

    Add some routes

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from re import L
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, 'views')
 
@@ -6,6 +7,21 @@ views = Blueprint(__name__, 'views')
 def home():
     return render_template('index.html', name = 'Aggie')
 
-@views.route('/profile/<username>')
-def profile(username):
-    return render_template('index.html', name = username)
+@views.route('/profile/')
+def profile():
+    args = request.args
+    name = args.get('name')
+    return render_template('index.html', name = name)
+
+@views.route('/json')
+def get_json():
+    return jsonify({'name': 'tim', 'something': 'else'})
+
+@views.route('/data')
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@view.route('/go-to-home')
+def go_to_home():
+    return redirect(url_for('views.get_json'))
